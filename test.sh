c08c720d33a4524008decfb6e5def0f534bfb6e4
Jakob Jensen <jakobj@jakobj.dk>
2022-12-10 23:50:42 +0100

    More apeacement

--- test.sh
+++ test.sh
@@ -2,5 +2,6 @@
 printf '#!/bin/bash\n' >/tmp/run.sh
 yq '.runs[].0.run' <action.yaml >>/tmp/run.sh
 chmod +x /tmp/run.sh
-shellcheck /tmp/run.sh .github/workflows/test-wrapper.sh $(find .github/workflows/tests -name "*.sh" -type f)
+# shellcheck disable=SC2046
+shellcheck test.sh /tmp/run.sh .github/workflows/test-wrapper.sh $(find .github/workflows/tests -name "*.sh" -type f)
 find .github/workflows/tests -name "*.sh" -type f -exec sh -c 'printf "\nTest %s:\n" "$(basename $1 ".sh")"; sh .github/workflows/test-wrapper.sh "$1" /tmp/run.sh' -- {} \;
